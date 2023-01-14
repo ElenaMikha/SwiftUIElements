c1f7ed22b50dbb680be39c4141aeb2da4da5029e
Elena <elmikhailova86@gmail.com>
2023-01-14 19:19:33 +0300

    Initial Commit

---
+++ SwiftUIElements/SwiftUIElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIElementsApp.swift
+//  SwiftUIElements
+//
+//  Created by Elena Mikhailova on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
